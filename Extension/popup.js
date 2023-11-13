d069bc2ec92ec617f806f3cb876a416a4f1e1350
kooroshkz <kooroshkz@yahoo.com>
2023-11-13 23:47:45 +0100

    configured on login uasccess page

--- Extension/popup.js
+++ Extension/popup.js
@@ -1,38 +1,58 @@
 // popup.js
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   const currentTab = tabs[0];
-  const isGoogle = currentTab.url.includes("google.com");
-  const storedKey = localStorage.getItem("totpKey");
+  const isULCN = currentTab.url.startsWith("https://login.uaccess.leidenuniv.nl");
+  const storedUsername = localStorage.getItem("Username");
+  const storedPassword = localStorage.getItem("Password");
+  const storedSecretKey = localStorage.getItem("Secret_Key");
 
   const statusElement = document.getElementById("status");
   const userInputElement = document.getElementById("userInput");
+  const passwordInputElement = document.getElementById("passwordInput");
+  const secretKeyInputElement = document.getElementById("secretKeyInput");
   const saveButton = document.getElementById("saveButton");
 
-  if (isGoogle) {
-    if (storedKey) {
+  if (isULCN) {
+    if (storedUsername && storedPassword && storedSecretKey) {
       const totp = new jsOTP.totp();
-      const totpCode = totp.getOtp(storedKey);
+      const totpCode = totp.getOtp(storedSecretKey);
       statusElement.textContent = "Current TOTP Code: " + totpCode;
       userInputElement.style.display = "none";
+      passwordInputElement.style.display = "none";
+      secretKeyInputElement.style.display = "none";
       saveButton.style.display = "none";
     } else {
-      statusElement.textContent = "No TOTP Key stored. Enter one:";
+      statusElement.textContent = "Enter your ULCN credentials and TOTP secret key:";
       userInputElement.style.display = "block";
+      passwordInputElement.style.display = "block";
+      secretKeyInputElement.style.display = "block";
       saveButton.style.display = "block";
     }
   } else {
-    statusElement.textContent = "You're not on Google";
+    statusElement.textContent = "You're not on ULCN login page";
     userInputElement.style.display = "none";
+    passwordInputElement.style.display = "none";
+    secretKeyInputElement.style.display = "none";
     saveButton.style.display = "none";
   }
 });
 
-function saveKey() {
-  const totpKey = document.getElementById("userInput").value;
-  localStorage.setItem("totpKey", totpKey);
+document.getElementById("saveButton").addEventListener("click", saveCredentials);
+
+function saveCredentials() {
+  const username = document.getElementById("userInput").value;
+  const password = document.getElementById("passwordInput").value;
+  const secretKey = document.getElementById("secretKeyInput").value;
+
+  localStorage.setItem("Username", username);
+  localStorage.setItem("Password", password);
+  localStorage.setItem("Secret_Key", secretKey);
+
   const totp = new jsOTP.totp();
-  const totpCode = totp.getOtp(totpKey);
+  const totpCode = totp.getOtp(secretKey);
   document.getElementById("status").textContent = "Current TOTP Code: " + totpCode;
   document.getElementById("userInput").style.display = "none";
+  document.getElementById("passwordInput").style.display = "none";
+  document.getElementById("secretKeyInput").style.display = "none";
   document.getElementById("saveButton").style.display = "none";
 }
