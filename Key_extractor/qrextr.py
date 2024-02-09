2df8ea7820c7f30ab2c4ec925f3135d5fcc4f748
Koorosh Ubuntu <kkomeilizadeh@gmail.com>
2024-02-09 13:54:39 +0100

    qrextr.py (Key extractor script) error handler added.

--- Key_extractor/qrextr.py
+++ Key_extractor/qrextr.py
@@ -5,27 +5,30 @@ from PIL import ImageGrab
 import io
 import pyotp
 
-# Read the image from the clipboard
-image = ImageGrab.grabclipboard()
-image_bytes = io.BytesIO()
-image.save(image_bytes, format='PNG')
-image_bytes = image_bytes.getvalue()
+try:
+    # Read the image from the clipboard
+    image = ImageGrab.grabclipboard()
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format='PNG')
+    image_bytes = image_bytes.getvalue()
 
-# Convert to a format readable by cv2
-image_np = np.frombuffer(image_bytes, dtype=np.uint8)
-image_cv = cv2.imdecode(image_np, flags=cv2.IMREAD_COLOR)
+    # Convert to a format readable by cv2
+    image_np = np.frombuffer(image_bytes, dtype=np.uint8)
+    image_cv = cv2.imdecode(image_np, flags=cv2.IMREAD_COLOR)
 
-# Decode the QR code
-detector = cv2.QRCodeDetector()
-data, vertices_array, binary_qrcode = detector.detectAndDecode(image_cv)
+    # Decode the QR code
+    detector = cv2.QRCodeDetector()
+    data, vertices_array, binary_qrcode = detector.detectAndDecode(image_cv)
 
-start = data.find('secret=') + len('secret=')
-end = data.find('&', start)
-secret = data[start:end]
+    start = data.find('secret=') + len('secret=')
+    end = data.find('&', start)
+    secret = data[start:end]
 
-# Generate a TOTP code
-totp = pyotp.TOTP(secret)
-totp_code = totp.now()
+    # Generate a TOTP code
+    totp = pyotp.TOTP(secret)
+    totp_code = totp.now()
 
+    print(f"Secret key = {secret} \n TOTP Code = {totp_code}")
 
-print(f"Secret key = {secret} \n TOTP Code = {totp_code}")
+except Exception as e:
+    print(f"**The image didnt found in clipboard or unreadable**\nError occurred: {str(e)}")
